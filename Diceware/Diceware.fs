f30f5d859691b6301b3b82829685ccf7b4d7e1a0
Damon McMinn <damon@kinjarling.org>
2016-12-10 16:30:53 +0000

    Refactor for readability
    - Make clear intent
    - Add link refs for things that required looking up

--- Diceware/Diceware.fs
+++ Diceware/Diceware.fs
@@ -3,42 +3,47 @@ module Diceware
 open System
 
 module Util =
-    let wordList =
+    let words =
         let lineToTuple (line:string) =
             line.Split('\t')
+            // http://stackoverflow.com/a/2197433
             |> function [|key; word|] -> key, word | _ -> failwith "Shutting up the warning"
-
+        
+        // http://stackoverflow.com/a/17191676
         IO.Path.GetDirectoryName(Reflection.Assembly.GetExecutingAssembly().Location)
         |> sprintf "%s/eff_large_wordlist.txt"
         |> System.IO.File.ReadLines
         |> Seq.map lineToTuple
         |> Map.ofSeq
 
-    let generatePhrase rolls =
-        rolls
-        |> Seq.map (fun key -> Map.find key wordList)
-        |> fun words -> String.Join(" ", words)
+    let getPhraseLength argv =
+        let defaultPhraseLength = 4
 
-    let parseCliArgs argv =
         match Array.tryHead argv with
-        | None -> 4
-        | Some i ->
-            match Int32.TryParse i with
-            | _, 0 -> 4
-            | _, n -> n
+        | None -> defaultPhraseLength
+        | Some arg ->
+            match Int32.TryParse arg with
+            | true, i -> i
+            | _ -> defaultPhraseLength
 
 module Dice =
-    let singleRoll (rand:Random) =
-        seq { for i in 1..5 -> rand.Next(1, 6).ToString()}
+    let rollOnce (rand:Random) =
+        seq { for _ in 1..5 -> rand.Next(1, 6).ToString() }
         |> String.Concat
 
-    let rollDice num =
+    let rollNumberOfTimes num =
         let rand = new Random()
-        seq { for _ in 1..num -> singleRoll rand }
+        seq { for _ in 1..num -> rollOnce rand }
+
+let generatePhrase rolls =
+    rolls
+    |> Seq.map (fun key -> Map.find key Util.words)
+    |> fun words -> String.Join(" ", words)
 
 [<EntryPoint>]
 let main argv =
-    let numWords = Util.parseCliArgs argv
+    Util.getPhraseLength argv
+    |> (Dice.rollNumberOfTimes >> generatePhrase)
+    |> printfn "%s"
 
-    printfn "%s" ((Dice.rollDice >> Util.generatePhrase) numWords)
     0 // return an integer exit code
