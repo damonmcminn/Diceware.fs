6fbb678590061c0c32dfef4c30f1ae7e127b4c10
Damon McMinn <damon@kinjarling.org>
2016-12-10 10:20:27 +0000

    Initial commit.

---
+++ Diceware/Diceware.fs
@@ -0,0 +1,46 @@
+module Diceware
+
+open System
+
+module Util =
+    let wordList =
+        let lineToTuple (line:string) =
+            line.Split('\t')
+            |> function [|key; word|] -> key, word | _ -> failwith "Shutting up the warning"
+
+        IO.Path.GetDirectoryName(Reflection.Assembly.GetExecutingAssembly().Location)
+        |> sprintf "%s/eff_large_wordlist.txt"
+        |> System.IO.File.ReadLines
+        |> Seq.map lineToTuple
+        |> Map.ofSeq
+
+    let generatePhrase rolls =
+        rolls
+        |> Seq.map (fun key -> Map.find key wordList)
+        |> fun words -> String.Join(" ", words)
+
+    let parseCliArgs argv =
+        match Array.tryHead argv with
+        | None -> 4
+        | Some i ->
+            match Int32.TryParse i with
+            | _, 0 -> 4
+            | _, n -> n
+
+module Dice =
+    let singleRoll (rand:Random) =
+        seq { for i in 1..5 -> rand.Next(1, 6).ToString()}
+        |> String.Concat
+        // |> fun key -> Map.find key wordList
+
+    let rollDice num =
+        let rand = new Random()
+        seq { for _ in 1..num -> singleRoll rand }
+        // |> fun words -> String.Join(" ", words)
+
+[<EntryPoint>]
+let main argv =
+    let numWords = Util.parseCliArgs argv
+
+    printfn "%s" ((Dice.rollDice >> Util.generatePhrase) numWords)
+    0 // return an integer exit code
---
+++ build.fsx
@@ -0,0 +1,43 @@
+// include Fake libs
+#r "./packages/FAKE/tools/FakeLib.dll"
+
+open Fake
+
+// Directories
+let buildDir  = "./build/"
+let deployDir = "./deploy/"
+
+
+// Filesets
+let appReferences  =
+    !! "/**/*.csproj"
+    ++ "/**/*.fsproj"
+
+// version info
+let version = "0.1"  // or retrieve from CI server
+
+// Targets
+Target "Clean" (fun _ ->
+    CleanDirs [buildDir; deployDir]
+)
+
+Target "Build" (fun _ ->
+    FileUtils.cp "./Diceware/eff_large_wordlist.txt" buildDir
+    // compile all projects below src/app/
+    MSBuildDebug buildDir "Build" appReferences
+    |> Log "AppBuild-Output: "
+)
+
+Target "Deploy" (fun _ ->
+    !! (buildDir + "/**/*.*")
+    -- "*.zip"
+    |> Zip buildDir (deployDir + "ApplicationName." + version + ".zip")
+)
+
+// Build order
+"Clean"
+  ==> "Build"
+  ==> "Deploy"
+
+// start build
+RunTargetOrDefault "Build"
